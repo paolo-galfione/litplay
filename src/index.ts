9def59304e55b1bc898b3222681d35a6ff59b391
unknown <paolo.galfione@softwareuno.it>
2020-01-03 16:22:02 +0100

    add lit-element

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
+import { customElement, LitElement, html } from 'lit-element';
 
 let msg:string;
 msg = "Paolo"
 
 console.log(msg);
 
+@customElement("my-element")
+export class MyElement extends LitElement {
+
+    render() {
+        return html`<h1>My Element<h1>`;
+    }
+}
